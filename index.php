4f0b0377418b0ef0cac30a0d90e3f5761cc35605
Phillip Ball <pball@mail.greenriver.edu>
2021-04-27 23:39:47 -0400

    Created 3 pages to be forms and a page to be a summary as well as their css files. Added routes for each new page to the controller.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  * Phillip Ball
  * 04/21/2021
  * This is my controller for the Dating 1 assignment
+ * Additional work began on 04/27/2021 for Dating 2
 */
 
 // turn on error-reporting
@@ -16,11 +17,39 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 // define routes
-// default route
+// default (home) route
 $f3->route('GET /', function (){
     // display the home page
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+// part 1 of the create a profile form
+$f3->route('GET /personalInfo', function (){
+    // display the form part 1 "Personal Information"
+    $view = new Template();
+    echo $view->render('views/personalInfo.html');
+});
+
+// part 2 of the create a profile form
+$f3->route('GET /profile', function (){
+    // display the form part 1 "Profile"
+    $view = new Template();
+    echo $view->render('views/profile.html');
+});
+
+// part 3 of the create a profile form
+$f3->route('GET /interests', function (){
+    // display the form part 1 "Interests"
+    $view = new Template();
+    echo $view->render('views/interests.html');
+});
+
+// summary of entered info from form
+$f3->route('GET /summary', function (){
+    // display the form summary
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 // run Fat-Free
