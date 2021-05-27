7ccbb9bf04db113886ca40e1afe7f551ec7f3bd3
Phillip Ball <pball@mail.greenriver.edu>
2021-05-26 20:19:46 -0400

    Created Member and PremiumMember classes.

---
+++ classes/member.php
@@ -0,0 +1,184 @@
+<?php
+/*
+ * 328/dating/classes/member.php
+ * Phillip Ball
+ * 05/26/2021
+ *
+ * This file is used to store users input as an object
+*/
+
+class Member
+{
+    // fields
+    private $_fname; // String
+    private $_lname; // String
+    private $_age; // int
+    private $_gender; // String
+    private $_phone; // String
+    private $_email; // String
+    private $_state; // String
+    private $_seeking; // String
+    private $_bio; // String
+
+    // methods
+    /**
+     * Member constructor.
+     * @param $_fname - String
+     * @param $_lname - String
+     * @param $_age - int
+     * @param $_gender - String
+     * @param $_phone - String
+     */
+    public function __construct($_fname, $_lname, $_age, $_gender, $_phone)
+    {
+        $this->_fname = $_fname;
+        $this->_lname = $_lname;
+        $this->_age = $_age;
+        $this->_gender = $_gender;
+        $this->_phone = $_phone;
+    }
+
+    /**
+     * @return mixed String
+     */
+    public function getFname()
+    {
+        return $this->_fname;
+    }
+
+    /**
+     * @param mixed $fname
+     */
+    public function setFname($fname)
+    {
+        $this->_fname = $fname;
+    }
+
+    /**
+     * @return mixed String
+     */
+    public function getLname()
+    {
+        return $this->_lname;
+    }
+
+    /**
+     * @param mixed $lname
+     */
+    public function setLname($lname)
+    {
+        $this->_lname = $lname;
+    }
+
+    /**
+     * @return mixed int
+     */
+    public function getAge()
+    {
+        return $this->_age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->_age = $age;
+    }
+
+    /**
+     * @return mixed String
+     */
+    public function getGender()
+    {
+        return $this->_gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->_gender = $gender;
+    }
+
+    /**
+     * @return mixed String
+     */
+    public function getPhone()
+    {
+        return $this->_phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->_phone = $phone;
+    }
+
+    /**
+     * @return mixed String
+     */
+    public function getEmail()
+    {
+        return $this->_email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->_email = $email;
+    }
+
+    /**
+     * @return mixed String
+     */
+    public function getState()
+    {
+        return $this->_state;
+    }
+
+    /**
+     * @param mixed $state
+     */
+    public function setState($state)
+    {
+        $this->_state = $state;
+    }
+
+    /**
+     * @return mixed String
+     */
+    public function getSeeking()
+    {
+        return $this->_seeking;
+    }
+
+    /**
+     * @param mixed $seeking
+     */
+    public function setSeeking($seeking)
+    {
+        $this->_seeking = $seeking;
+    }
+
+    /**
+     * @return mixed String
+     */
+    public function getBio()
+    {
+        return $this->_bio;
+    }
+
+    /**
+     * @param mixed $bio
+     */
+    public function setBio($bio)
+    {
+        $this->_bio = $bio;
+    }
+}
---
+++ classes/premiumMember.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * 328/dating/classes/premiumMember.php
+ * Phillip Ball
+ * 05/26/2021
+ *
+ * This file is a child class of 328/dating/classes/member.php
+*/
+
+class PremiumMember extends Member
+{
+    // fields
+    private $_inDoorInterests = array();
+    private $_outDoorInterests = array();
+
+    // methods
+    /**
+     * @return array of Strings
+     */
+    public function getInDoorInterests()
+    {
+        return $this->_inDoorInterests;
+    }
+
+    /**
+     * @param array $inDoorInterests
+     */
+    public function setInDoorInterests($inDoorInterests)
+    {
+        $this->_inDoorInterests = $inDoorInterests;
+    }
+
+    /**
+     * @return array of Strings
+     */
+    public function getOutDoorInterests()
+    {
+        return $this->_outDoorInterests;
+    }
+
+    /**
+     * @param array $outDoorInterests
+     */
+    public function setOutDoorInterests($outDoorInterests)
+    {
+        $this->_outDoorInterests = $outDoorInterests;
+    }
+}
